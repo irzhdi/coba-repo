da6d204dc5482481b71a65a13654080d389b5ee5
irzhdi <aliffffffff@gmail.com>
2022-02-25 14:16:36 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package com.juaracoding.main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.print(" Juaracoding");
+		
+	 
+		
+			   
+		  
+	}
+
+}
